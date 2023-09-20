ee610a5cc62856b98760651de6f8da25a0d61bde
louis.heraut <louis.heraut@inrae.fr>
2023-09-20 20:01:57 +0200

    france add river

--- shp2json.R
+++ shp2json.R
@@ -23,15 +23,9 @@ computer_data_path = "/home/louis/Documents/bouleau/INRAE/data/"
 map_dir = "map"
 data_dir = "data"
 
-france = st_read(file.path(computer_data_path, map_dir, "france"))
-# france = st_simplify(france,
-                     # preserveTopology=TRUE,
-                     # dTolerance=1000)
-
-
+# france
 output = file.path(data_dir, "france.geo.json")
-
-
+france = st_read(file.path(computer_data_path, map_dir, "france"))
 bbox = st_bbox(france)
 
 json_data = list(
@@ -62,18 +56,52 @@ json_data = list(
     )
 )
 
-
 json_string = toJSON(json_data,
                      pretty=TRUE,
                      auto_unbox=TRUE)
 writeLines(json_string, output)
 
 
+# river
+output = file.path(data_dir, "river.geo.json")
+river = st_read(file.path(computer_data_path, map_dir, "river"))
+river = st_transform(river, 4326)
+river$length = as.numeric(st_length(river$geometry))
+river = filter(river, length >= 100000)
 
+nRiver = length(river$geometry)
+# nRiver = 10
 
-# json_string = fromJSON("data/Australia and New Zealand.geo.json")
-# json_string = toJSON(json_string,
-#                      pretty=TRUE,
-#                      auto_unbox=TRUE)
-# writeLines(json_string,
-#            "data/Australia and New Zealand_2.geo.json")
+json_data = list(
+    type="FeatureCollection",
+    
+    features = lapply(
+        1:nRiver,
+        function (i) {
+            list(
+                type="Feature",
+                
+                properties=list(
+                    code=river$CdEntiteHy[i],
+                    name=river$NomEntiteH[i],
+                    length=river$length[i]
+                ),
+                
+                geometry=list(
+                    type="MultiLineString",
+                    coordinates=
+                        lapply(        
+                            1:length(river$geometry[[i]]),
+                            function (j) {
+                                river$geometry[[i]][[j]][, c(1, 2)]
+                            })
+                )
+            )
+        })
+)
+
+json_string = toJSON(json_data,
+                     pretty=TRUE,
+                     auto_unbox=TRUE)
+writeLines(json_string, output)
+
